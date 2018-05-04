2d9da2c8b708c093476e22c36bd3827bd490387e
Connor Christensen <chriconn@oregonstate.edu>
2018-05-03 17:43:35 -0700

    Added in uiDark and uiLight scripts

---
+++ UI/uiLight.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# hide the dark widgets in the ui folder
+osascript /usr/local/bin/scripts/ui/hideDarkWidgets.scpt
+# show the light widgets in the ui folder
+osascript /usr/local/bin/scripts/ui/showLightWidgets.scpt
+
+osascript /usr/local/bin/scripts/ui/changeBackgroundPhotoRandomLight.scpt
