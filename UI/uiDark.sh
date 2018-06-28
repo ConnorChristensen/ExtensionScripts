59ca28f20d89109d0d14f6005f930137928930e1
Connor Christensen <chriconn@oregonstate.edu>
2018-06-27 22:12:08 -0700

    Updated UI folder to use pywal

--- UI/uiDark.sh
+++ UI/uiDark.sh
@@ -4,4 +4,12 @@ osascript /usr/local/bin/scripts/ui/hideLightWidgets.scpt
 # show the dark widgets in the ui folder
 osascript /usr/local/bin/scripts/ui/showDarkWidgets.scpt
 
-osascript /usr/local/bin/scripts/ui/changeBackgroundPhotoRandomDark.scpt
+# get our image folder
+folder=~/Pictures/background/dark/*
+
+# select the first line after random sorting
+# to get a random image from that folder
+image=$(ls $folder | sort -R | head -n 1)
+
+# change the background and the color scheme
+wal -i "$image"
--- UI/uiLight.sh
+++ UI/uiLight.sh
@@ -4,4 +4,12 @@ osascript /usr/local/bin/scripts/ui/hideDarkWidgets.scpt
 # show the light widgets in the ui folder
 osascript /usr/local/bin/scripts/ui/showLightWidgets.scpt
 
-osascript /usr/local/bin/scripts/ui/changeBackgroundPhotoRandomLight.scpt
+# get our image folder
+folder=~/Pictures/background/light/*
+
+# select the first line after random sorting
+# to get a random image from that folder
+image=$(ls $folder | sort -R | head -n 1)
+
+# change the background and the color scheme
+wal -i "$image"
